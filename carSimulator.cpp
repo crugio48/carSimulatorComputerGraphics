63ce122caab2ff9b72f661bd070ecb6aa5e613cb
crugio48 <matteo2.crugnola@mail.polimi.it>
2022-07-14 16:31:58 +0200

    done instance

--- carSimulator.cpp
+++ carSimulator.cpp
@@ -9,41 +9,74 @@ const uint32_t WIDTH = 800;
 const uint32_t HEIGHT = 600;
 
 class CarSimulatorApp {
-    public:
-        void run() {
-            initWindow();
-            initVulkan();
-            mainLoop();
-            cleanup();
+public:
+    void run() {
+        initWindow();
+        initVulkan();
+        mainLoop();
+        cleanup();
+    }
+
+private:
+    GLFWwindow* window;
+
+    VkInstance instance;
+
+
+    void initWindow() {
+        glfwInit();
+
+        glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+        window = glfwCreateWindow(WIDTH, HEIGHT, "Car simulator", nullptr, nullptr);
+    }
+
+    void initVulkan() {
+        createInstance();
+
+    }
+
+    void createInstance() {
+        VkApplicationInfo appInfo{};
+        appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+        appInfo.pApplicationName = "Hello Triangle";
+        appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.pEngineName = "No Engine";
+        appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.apiVersion = VK_API_VERSION_1_0;
+
+        VkInstanceCreateInfo createInfo{};
+        createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+        createInfo.pApplicationInfo = &appInfo;
+
+        uint32_t glfwExtensionCount = 0;
+        const char** glfwExtensions;
+        glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+        createInfo.enabledExtensionCount = glfwExtensionCount;
+        createInfo.ppEnabledExtensionNames = glfwExtensions;
+
+        createInfo.enabledLayerCount = 0;
+
+        if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS) {
+            throw std::runtime_error("failed to create instance!");
         }
-    
-    private:
-        GLFWwindow* window;
+    }
 
-        void initWindow() {
-            glfwInit();
-
-            glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-            glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
-
-            window = glfwCreateWindow(WIDTH, HEIGHT, "Car simulator", nullptr, nullptr);
+    void mainLoop() {
+        while (!glfwWindowShouldClose(window)) {
+            glfwPollEvents();
         }
+    }
 
-        void initVulkan() {
+    void cleanup() {
+        vkDestroyInstance(instance, nullptr);
 
-        }
+        glfwDestroyWindow(window);
 
-        void mainLoop() {
-            while (!glfwWindowShouldClose(window)) {
-                glfwPollEvents();
-            }
-        }
-
-        void cleanup() {
-            glfwDestroyWindow(window);
-
-            glfwTerminate();
-        }
+        glfwTerminate();
+    }
 };
 
 int main() {
